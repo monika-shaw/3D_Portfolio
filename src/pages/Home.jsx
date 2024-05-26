c5ede998f3f5519797a1872ffec3e9757213117d
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-26 14:20:54 +0530

    Description : Rotating clouds. Developer : Monika.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                     <ambientLight intensity={0.5} />
                     <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
                     <Bird />
-                    <Sky />
+                    <Sky isRotating = {isRotating}/>
                     <Island
                         position={isLandPosition}
                         scale={isLandScale}
