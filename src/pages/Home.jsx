0aec522bb89e230021f4da113d0e43ebdca649af
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-26 08:58:35 +0530

    Description : Implemented loader, lights to render 3D scenes. Developer : Monika.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,21 @@ import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import Loader from "../components/Loader"
 const Home = () => {
-  return (
-    <section className="w-full h-screen relative">
-        <Canvas className="w-full h-screen bg-transparent"
-        camera={{near:0.1, far:1000}}
-        >
-            <Suspense fallback={<Loader/>}>
-
-            </Suspense>
-        </Canvas>
-    </section>
-  )
+    return (
+        <section className="w-full h-screen relative">
+            <Canvas className="w-full h-screen bg-transparent"
+                camera={{ near: 0.1, far: 1000 }}
+            >
+                <Suspense fallback={<Loader />}>
+                    <directionalLight />
+                    <spotLight />
+                    <ambientLight />
+                    <pointLight />
+                    <hemisphereLight />
+                </Suspense>
+            </Canvas>
+        </section>
+    )
 }
 
 export default Home
