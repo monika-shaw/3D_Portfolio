89e4a0d4a74b305ebbb33e7e4724ee14fe5c0efb
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-26 22:02:24 +0530

    Description : Functionality for fox and its animation Completed. Developer : Monika.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
-import { useRef, useState } from "react"
+import { Suspense, useRef, useState } from "react"
 import emailjs from "@emailjs/browser"
+import { Canvas } from "@react-three/fiber"
+import Fox from "../models/Fox"
+import Loader from "../components/Loader"
+
 const Contact = () => {
     const formRef = useRef()
     const [form, setForm] = useState({
@@ -7,17 +11,20 @@ const Contact = () => {
     })
 
     const [isLoading, setIsLoading] = useState(false)
+    const [currentAnimation, setCurrentAnimation] = useState('idle')
+
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
-    const handleFocus = () => { }
+    const handleFocus = () => setCurrentAnimation('walk')
 
-    const handleBlur = () => { }
+    const handleBlur = () => setCurrentAnimation('idle')
 
     const handleSubmit = (e) => {
         e.preventDefault()
         setIsLoading(true)
+        setCurrentAnimation('hit')
         emailjs.send(
             import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
             import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
@@ -31,9 +38,14 @@ const Contact = () => {
             import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
         ).then(() => {
             setIsLoading(false)
-            setForm({ ...form, [e.target.name]: e.target.value })
+            setTimeout(() => {
+                setCurrentAnimation('idle')
+                setForm({ name: '', email: '', message: '' })
+            }, [3000]);
+
         }).catch((err) => {
             setIsLoading(false)
+            setCurrentAnimation('idle')
             console.log(err);
         })
     }
@@ -93,6 +105,32 @@ const Contact = () => {
                         {isLoading ? 'Sending...' : 'Send Message'}
                     </button>
                 </form>
+            </div>
+            <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+                <Canvas
+                    camera={{
+                        position: [0, 0, 5],
+                        fov: 75,
+                        near: 0.1,
+                        far: 1000
+                    }}
+                >
+                    <directionalLight
+                        intensity={2.5}
+                        position={[0, 0, 1]}
+                    />
+                    <ambientLight
+                        intensity={0.5}
+                    />
+                    <Suspense fallback={<Loader />}>
+                        <Fox
+                            currentAnimation={currentAnimation}
+                            position={[0.5, 0.35, 0]}
+                            rotation={[12.6, -0.6, 0]}
+                            scale={[0.5, 0.5, 0.5]}
+                        />
+                    </Suspense>
+                </Canvas>
             </div>
         </section>
     )
