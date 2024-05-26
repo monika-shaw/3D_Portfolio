67787037aa2f970bf1562732c719bdb75c005b0d
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-26 11:08:21 +0530

    Description : Renderd 3D plane, Bird and sky. Developer : Monika.

---
+++ src/models/Plane.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import planeScene from '../assets/3d/plane.glb'
+import { useGLTF } from '@react-three/drei'
+
+export const Plane = () => {
+    const { scene, animations } = useGLTF(planeScene)
+    return (
+        <mesh>
+            <primitive object={scene} />
+        </mesh>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Suspense } from "react"
 import Loader from "../components/Loader"
 import Island from "../models/Island"
 import Sky from "../models/Sky"
+import { Bird } from "../models/Bird"
+import Plane from "../models/Plane"
 
 const Home = () => {
     const adjustIslandForScreenSize = () => {
@@ -24,15 +26,17 @@ const Home = () => {
                 camera={{ near: 0.1, far: 1000 }}
             >
                 <Suspense fallback={<Loader />}>
-                    <directionalLight position={[1,1,1]} intensity={1} />
-                    <ambientLight intensity={0.5}/>
-                    <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-                    <Sky/>
-                    <Island 
-                    position = {isLandPosition}
-                    scale={isLandScale}
-                    rotation = {isLandRotation}
+                    <directionalLight position={[1, 1, 1]} intensity={1} />
+                    <ambientLight intensity={0.5} />
+                    <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+                    <Bird />
+                    <Sky />
+                    <Island
+                        position={isLandPosition}
+                        scale={isLandScale}
+                        rotation={isLandRotation}
                     />
+                    <Plane />
                 </Suspense>
             </Canvas>
         </section>
