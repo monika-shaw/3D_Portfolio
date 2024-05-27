38578a135c7a90447981718aac702ba3aa344f31
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-27 23:15:02 +0530

    Description : added sound. Developer : Monika.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,27 @@ import Sky from "../models/Sky"
 import Bird from "../models/Bird"
 import Plane from "../models/Plane"
 import HomeInfo from "../components/HomeInfo"
+import sakura from '../assets/sakura.mp3'
+import { useEffect, useRef } from "react"
+import { soundoff, soundon } from "../assets/icons"
 
 const Home = () => {
+    const audioRef = useRef(new Audio(sakura))
+    audioRef.current.volume = 1
+    audioRef.current.loop = true
     const [isRotating, setIsRotating] = useState(false)
     const [currentStage, setCurrentStage] = useState(1)
+    const [isPlayingMusic, setIsPlayingMusic] = useState(false)
+
+    useEffect(() => {
+        if (isPlayingMusic)
+            audioRef.current.play()
+
+        return () => {
+            audioRef.current.pause()
+        }
+    }, [isPlayingMusic])
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
@@ -67,6 +84,15 @@ const Home = () => {
                     />
                 </Suspense>
             </Canvas>
+            <div className="absolute bottom-2 left-2">
+                <img
+                    src={!isPlayingMusic ? soundoff : soundon}
+                    alt="sound"
+                    className="w-10 h-10 cursor-pointer object-contain"
+                    onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+
+                />
+            </div>
         </section>
     )
 }
